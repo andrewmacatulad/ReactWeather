6b347462b0f4011d48dea16339c7bf4dad04720f
andrewmacatulad@gmail.com <andrewmacatulad@gmail.com>
2017-03-22 23:19:56 +0800

    for localhost

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const PORT = process.env.PORT || 3000;
 // express middleware sample
 app.use(function(req, res, next){
   // x-forwarded-proto is either https or http
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  } else {
+  if(req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 })
 app.use(express.static('public'));
